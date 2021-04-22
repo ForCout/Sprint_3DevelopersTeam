b637b91bdc8b4a544939c200ca6f287d8efddc72
Pilar M <pilarmingh@gmail.com>
2021-04-22 16:28:22 +0200

    Bucle del menú

--- app.js
+++ app.js
@@ -1,52 +1,64 @@
 const inquirer = require("inquirer");
 let t = require("./tareas.js");
 
-inquirer
-    .prompt({
-      type: "rawlist",
-      name: "opcion",
-      message: "Opcion elegida :",
-      choices: [
-        "crear",
-        "actualizar",
-        "Borrar",
-        "Listar todas",
-        "Lista por Id",
-        "salir",
-      ],
-    })
-
-  .then(({ opcion }) => {
-    if (opcion === "crear") {
-      t.crear();
-      
-    } else if (opcion === "Borrar") {
-      inquirer.prompt(
-        {
-          name: "id",
-          message: "Entra el id?",
-          type: "input"
-        })
-        .then(({ id }) => {
-          t.eliminar(id);
-
-         })
-      
-    } else if (opcion === "Listar todas") {
-      t.listarTareas();
-    } else if (opcion === "Lista por Id") {
-      inquirer.prompt(
-        {
-          name: "id",
-          message: "Entra el id?",
-          type: "input"
-        })
-        .then(({ id }) => {
-        console.log(id)
-        t.listarById(id);
+function loopMenu() {
+  const menu = () => {
+    inquirer
+      .prompt({
+        type: "rawlist",
+        name: "opcion",
+        message: "Opcion elegida :",
+        choices: [
+          "crear",
+          "actualizar",
+          "Borrar",
+          "Listar todas",
+          "Lista por Id",
+          "salir",
+        ],
       })
-        
-      }
-  
-})
+
+      .then(({ opcion }) => {
+        if (opcion === "crear") {
+          t.crear();
+          menu();
+
+        } else if (opcion === "Borrar") {
+          inquirer.prompt(
+            {
+              name: "id",
+              message: "Entra el id?",
+              type: "input"
+            })
+            .then(({ id }) => {
+              t.eliminar(id);
+              menu();
+
+            })
+
+        } else if (opcion === "Listar todas") {
+          t.listarTareas();
+          menu();
+        } else if (opcion === "Lista por Id") {
+          inquirer.prompt(
+            {
+              name: "id",
+              message: "Entra el id?",
+              type: "input"
+            })
+            .then(({ id }) => {
+              console.log(id)
+              t.listarById(id);
+              menu();
+
+            })
+
+        }
+      })
+  }
+  menu();
+
+}
+
+loopMenu();
 
